5808ebfec2e6d4ab5bcb14b4ea083cee986779a5
guangyuan-yang <guangyuan-yang>
2024-06-21 13:38:45 +0000

    Add Azure tests

--- Test_whisper_diarization.py
+++ Test_whisper_diarization.py
@@ -44,3 +44,88 @@ print('Done!')
 # COMMAND ----------
 
 !pip install git+https://github.com/MahmoudAshraf97/ctc-forced-aligner.git
+
+# COMMAND ----------
+
+! pip install protobuf~=3.20
+
+# COMMAND ----------
+
+script_path = "/dbfs/FileStore/shared_uploads/your-email@example.com/diarize.py"  # Adjust the path accordingly
+
+with open(script_path, 'r') as file:
+    script_content = file.read()
+
+exec(script_content)
+
+# COMMAND ----------
+
+import os
+import wget
+from omegaconf import OmegaConf
+import json
+import shutil
+import torch
+import torchaudio
+from nemo.collections.asr.models.msdd_models import NeuralDiarizer
+from deepmultilingualpunctuation import PunctuationModel
+import re
+import logging
+import nltk
+from whisperx.utils import LANGUAGES, TO_LANGUAGE_CODE
+from ctc_forced_aligner import (
+    load_alignment_model,
+    generate_emissions,
+    preprocess_text,
+    get_alignments,
+    get_spans,
+    postprocess_results,
+)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC pwd
+# MAGIC ls
+
+# COMMAND ----------
+
+dbfs_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().getOrElse(None)
+print(dbfs_path)
+
+# COMMAND ----------
+
+
+if dbfs_path is not None:
+    try:
+        dbutils.fs.ls(dbfs_path)
+        print("Path is valid.")
+    except Exception as e:
+        print("Path is invalid:", str(e))
+else:
+    print("DBFS path is not available.")
+
+# COMMAND ----------
+
+dbfs_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+
+
+# COMMAND ----------
+
+try:
+    dbutils.fs.ls(dbfs_path)
+    print("Path is valid.")
+except Exception as e:
+    print("Path is invalid:", str(e))
+
+# COMMAND ----------
+
+# MAGIC %sh python diarize.py -a '4065.mp3'
